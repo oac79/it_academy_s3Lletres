bc587675915dfc4c1cfa4722b2e54f84b9c12c6d
oli79 <oabadch@gmail.com>
2021-05-20 10:33:27 +0200

    sprint 3 nivell 1 exercici 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    //Nivell 1 exercici 1
+    let name = ['O', 'l', 'i', 'v', 'e', 'r'];
+    // for (let index = 0; index < array.length; index++) {
+    //     const element = array[index];
+    // }
+    name.forEach(element => {
+        console.log(element);
+    });
+})
