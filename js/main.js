57270033f6ba892d0c6d2a88985fa25a9af5eca0
oli79 <oabadch@gmail.com>
2021-05-20 11:33:34 +0200

    sprint 3 nivell 1 exercici 2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 $(function () {
     //Nivell 1 exercici 1
-    let name = ['O', 'l', 'i', 'v', 'e', 'r'];
+    let name = ['O', 'l', 'i', 'v', 'e', 'r', '7', '9'];
     // for (let index = 0; index < array.length; index++) {
     //     const element = array[index];
     // }
-    name.forEach(element => {
-        console.log(element);
+    name.forEach(letter => {
+        if (isVowel(letter)) {
+            console.log(letter + '__ es una vocal!');
+        } else if (isNumeric(letter)) {
+            console.log(letter + '__ el nombre no puede contener números!');
+        } else {
+            console.log(letter + '__ es una consonante!');
+        }
     });
+
+
 })
+
+function isVowel(letter) {
+    let letterLower = letter.toLowerCase();
+    if (letterLower == 'a' || letterLower == 'e' || letterLower == 'i' || letterLower == 'o' || letterLower == 'u') {
+        return true;
+    }
+    return false;
+}
+
+function isNumeric(letter) {
+    for (let i = 0; i <= 9; i++) {
+        if (letter == i) {
+            return true;
+        }
+    }
+    return false;
+}
